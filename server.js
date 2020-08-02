33336e0169a487feae1cdae9f8c175e13f3ca28e
Eduardo P. Silveira <edupsilveira@gmail.com>
2020-08-02 01:15:09 -0300

    Heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`JSON Server is running in ${port}`);
+});
+
+// no package.json
+// "server": "json-server --watch db.json --port 8080",
+// "start": "react-scripts start",
